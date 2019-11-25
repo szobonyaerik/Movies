abcf7d2310bfb9ceedf065b235bf50d0248602a6
Erik Szobonya <szobonyaerik@gmail.com>
2019-11-25 13:18:13 +0100

    add

--- Movies/Program.cs
+++ Movies/Program.cs
@@ -3,18 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Movies
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            int z;
-            x(ref z);
-        }
-
-        static void x(ref int x)
         {
         }
     }
