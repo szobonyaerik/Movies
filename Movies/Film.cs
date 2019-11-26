5fbd1e0e56bd8f259090a47fb70d6c10c4632972
Erik Szobonya <szobonyaerik@gmail.com>
2019-11-26 09:20:22 +0100

    k

---
+++ Movies/Film.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies
+{
+    class Film
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string ReleaseYear { get; set; }
+        public string Stars { get; set; }
+        public int Budget { get; set; }
+
+        public Film(string Title, string Director, string ReleaseYear, string Stars, int Budget)
+        {
+            this.Title = Title;
+            this.Director = Director;
+            this.ReleaseYear = ReleaseYear;
+            this.Stars = Stars;
+            this.Budget = Budget;
+        }
+
+
+    }
+}
