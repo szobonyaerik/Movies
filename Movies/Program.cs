449c7d33deb355c2242993d2e30e844c0f592abe
makakoocelot <makakoocelot@gmail.com>
2019-11-27 13:23:54 +0100

    clear menu

--- Movies/Program.cs
+++ Movies/Program.cs
@@ -10,38 +10,49 @@ namespace Movies
     {
         static void Main(string[] args)
         {
-            
-   
-            displayMenu(mainMenu());
-            Console.Write("Enter a number to enter a menu: ");
-            int menuNumber = Convert.ToInt32(Console.ReadLine());
 
-            if (menuNumber == 1)
+            while (true)
             {
-                filmHandle.displayFilms();
-            }
-            else if(menuNumber == 2)
-            {
-                Console.WriteLine(filmHandle.filmsByTitle());
-            }
-            else if(menuNumber == 3)
-            {
-                var newFilm = filmHandle.addFilm();
-                FileHandling.writeToFile("../../Films.ini", newFilm);
-            }
-            else if(menuNumber == 4)
-            {
-                var delFilm = filmHandle.deleteFilmByTitle();
-                FileHandling.writeToFile("../../Films.ini",delFilm);
-            }
-            else if(menuNumber == 5)
-            {
-                Environment.Exit(0);
+                
+                try
+                {
+                    displayMenu(mainMenu());
+                    Console.Write("Enter a number to enter a menu: ");
+                    int menuNumber = Convert.ToInt32(Console.ReadLine());
 
+                    if (menuNumber == 1)
+                    {
+                        filmHandle.displayFilms();
+                    }
+                    else if (menuNumber == 2)
+                    {
+                        Console.WriteLine(filmHandle.filmsByTitle());
+                    }
+                    else if (menuNumber == 3)
+                    {
+                        var newFilm = filmHandle.addFilm();
+                        FileHandling.writeToFile("../../Films.ini", newFilm);
+                    }
+                    else if (menuNumber == 4)
+                    {
+                        var delFilm = filmHandle.deleteFilmByTitle();
+                        FileHandling.writeToFile("../../Films.ini", delFilm);
+                    }
+                    else if (menuNumber == 5)
+                    {
+                        Environment.Exit(0);
+
+                    }
+                    else if (menuNumber > 5)
+                        Console.WriteLine("there is no such option");
+                }
+                catch (Exception)
+                {
+                        Console.WriteLine("Invalid input");
+                }
             }
 
 
-            Console.ReadLine();
 
 
         }
