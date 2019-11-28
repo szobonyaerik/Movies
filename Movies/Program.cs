2b17bc9f9c3b0980399ee450459e24db7706a6bb
makakoocelot <makakoocelot@gmail.com>
2019-11-28 10:30:53 +0100

    make the ddl file and all console.write/writeline or readline has been deleted in the dll and i put in the main program

--- Movies/Program.cs
+++ Movies/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FilmLibrary;
+using System.IO;
 
 namespace Movies
 {
@@ -11,48 +12,65 @@ namespace Movies
     {
         static void Main(string[] args)
         {
-
-            while (true)
+            Console.Write("Give me the ini file in which you want to work: ");
+            string file = Console.ReadLine();
+            if (File.Exists(file))
             {
-                
-                try
+                while (true)
                 {
-                    displayMenu(mainMenu());
-                    Console.Write("Enter a number to enter a menu: ");
-                    int menuNumber = Convert.ToInt32(Console.ReadLine());
 
-                    if (menuNumber == 1)
+                    try
                     {
-                        filmHandle.displayFilms();
-                    }
-                    else if (menuNumber == 2)
-                    {
-                        Console.WriteLine(filmHandle.filmsByTitle());
-                    }
-                    else if (menuNumber == 3)
-                    {
-                        var newFilm = filmHandle.addFilm();
-                        FileHandling.writeToFile("../../testFilms.ini", newFilm);
-                    }
-                    else if (menuNumber == 4)
-                    {
-                        var delFilm = filmHandle.deleteFilmByTitle();
-                        FileHandling.writeToFile("../../testFilms.ini", delFilm);
-                    }
-                    else if (menuNumber == 5)
-                    {
-                        Environment.Exit(0);
+                        displayMenu(mainMenu());
+                        Console.Write("Enter a number to enter a menu: ");
+
+                        int menuNumber = Convert.ToInt32(Console.ReadLine());
+
+                        if (menuNumber == 1)
+                        {
+                            Console.WriteLine(filmHandle.displayFilms(file));
+                        }
+                        else if (menuNumber == 2)
+                        {
+                            Console.Write("Give me the film title: ");
+                            Console.WriteLine(filmHandle.filmsByTitle(file,Console.ReadLine()));
+                        }
+                        else if (menuNumber == 3)
+                        {
+                            Console.Write("Give me the film title: ");
+                            string title = "[" + Console.ReadLine() + "]";
+                            string[] keywords = { "director", "release_year", "stars", "budget" };
+                            List<string> values = new List<string>();
+                            foreach(string element in keywords)
+                            {
+                                Console.Write($"Give me the {element}: ");
+                                values.Add(Console.ReadLine());
+                            }
+                            
+                            FileHandling.writeToFile(file, filmHandle.addFilm(file, title, keywords, values));
+                        }
+                        else if (menuNumber == 4)
+                        {
+                            Console.Write("Give me the title: ");
+                            string title = "[" + Console.ReadLine() + "]";
+                           
+                            FileHandling.writeToFile(file, filmHandle.deleteFilmByTitle(file, title));
+                        }
+                        else if (menuNumber == 5)
+                        {
+                            Environment.Exit(0);
+
+                        }
+                        else if (menuNumber > 5)
+                            Console.WriteLine("there is no such option");
 
                     }
-                    else if (menuNumber > 5)
-                        Console.WriteLine("there is no such option");
-                }
-                catch (Exception)
-                {
+                    catch (Exception)
+                    {
                         Console.WriteLine("Invalid input");
+                    }
                 }
             }
-
 
 
 
